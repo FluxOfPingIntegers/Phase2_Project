0e6493e8a8d8ae4e9c34eab519cc3d163fea0aba
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-05-14 18:36:12 -0400

    User class will instantiate and save with encrypted passwords

--- app/models/User.rb
+++ app/models/User.rb
@@ -4,6 +4,16 @@ class User < ActiveRecord::Base
   has_many :favoritequotes
   has_many :quotes, through: :favoritequotes
   has_many :characters, through: :favoritequotes
+  has_secure_password
+
+#  def password=(string)
+#    self.password_digest = BCrypt::Password.create(string)
+#  end
+#
+#  def authenticate(password)
+#    bcrypt = BCrypt::Password.new(self.password_digest)
+#    bcrypt == password
+#  end
 
 
 end
