0a0590ef51be59a80654e1c6e3e9a9bf30fd1947
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-05-14 08:17:59 -0400

    API /character method success

--- app/services/CharacterImporter.rb
+++ app/services/CharacterImporter.rb
@@ -1,8 +1,22 @@
 require 'pry'
+require 'dotenv/load'
+
 class CharacterImporter
 
   def self.seed
-    url = "https://the-one-api.dev/v2/."
+    url = "https://the-one-api.dev/v2/character"
+    uri = URI(url)
+
+    headers = {
+        'Authorization'=>"Bearer #{ENV['KEY']}",
+        'Content-Type' =>'application/json',
+        'Accept'=>'application/json'
+    }
+    
+    request = Net::HTTP.new(uri.host, uri.port)
+    request.use_ssl = true
+    response = request.get(uri.path, headers)
+
   end
 
 end
