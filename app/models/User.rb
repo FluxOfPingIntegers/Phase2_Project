e58aacf65d90ed035bf1cfaaf9badcf46606b256
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-05-15 10:20:24 -0400

    character quotes have been figured out

--- app/models/User.rb
+++ app/models/User.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   has_many :characters, through: :favoritequotes
   has_secure_password
   validates :username, uniqueness: :true # failure = self.errors.full_messages.to_sentence = "Username has already been taken" 
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Must be a valid email address format" } #needs testing.
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Must be a valid email address" } #needs testing.
 
 
 end
--- app/services/CharacterImporter.rb
+++ app/services/CharacterImporter.rb
@@ -17,7 +17,8 @@ class LOTRImporter
     request = Net::HTTP.new(uri.host, uri.port)
     request.use_ssl = true
     response = request.get(uri.path, headers)
-    @@character_json = JSON.parse(response.body)
+    character = JSON.parse(response.body)
+    binding.pry
     #stored as json
   end
 
@@ -34,7 +35,28 @@ class LOTRImporter
     request = Net::HTTP.new(uri.host, uri.port)
     request.use_ssl = true
     response = request.get(uri.path, headers)
-    @@quote_json = JSON.parse(response.body)
+    quotes = JSON.parse(response.body)
+
+    smeagol_quotes = quotes["docs"].select { |x| x["character"] == "5cd99d4bde30eff6ebccfe9e" }
+    sam_quotes = quotes["docs"].select { |x| x["character"] == "5cd99d4bde30eff6ebccfd0d" }
+    pippin_quotes = quotes["docs"].select { |x| x["character"] == "5cd99d4bde30eff6ebccfe2e" }
+    merry_quotes = quotes["docs"].select { |x| x["character"] == "5cd99d4bde30eff6ebccfc7c" }
+
+    aragorn_quotes = quotes["docs"].select { |x| x["character"] == "5cd99d4bde30eff6ebccfbe6" }
+    boromir_quotes = quotes["docs"].select { |x| x["character"] == "5cd99d4bde30eff6ebccfc57" }
+    legolas_quotes = quotes["docs"].select { |x| x["character"] == "5cd99d4bde30eff6ebccfd81" }
+    gandalf_quotes = quotes["docs"].select { |x| x["character"] == "5cd99d4bde30eff6ebccfea0" }
+    gimili_quotes = quotes["docs"].select { |x| x["character"] == "5cd99d4bde30eff6ebccfd23" }
+    frodo_quotes = quotes["docs"].select { |x| x["character"] == "5cd99d4bde30eff6ebccfc15" }
+    galadriel_quotes = quotes["docs"].select { |x| x["character"] == "5cd99d4bde30eff6ebccfd06" }
+    eowyn_quotes = quotes["docs"].select { |x| x["character"] == "5cdbdecb6dc0baeae48cfa59" }
+    arwen_quotes = quotes["docs"].select { |x| x["character"] == "5cd99d4bde30eff6ebccfc07" }
+    elrond_quotes = quotes["docs"].select { |x| x["character"] == "5cd99d4bde30eff6ebccfcc8" }
+    saruman_quotes = quotes["docs"].select { |x| x["character"] == "5cd99d4bde30eff6ebccfea4" }
+    bilbo_quotes = quotes["docs"].select { |x| x["character"] == "5cd99d4bde30eff6ebccfc38" }
+    treebeard_quotes = quotes["docs"].select { |x| x["character"] == "5cd9d533844dc4c55e47afed" }
+
+    binding.pry
     #stored as json
   end
 
