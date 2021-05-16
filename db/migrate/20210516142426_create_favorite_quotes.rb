7c9f30603aea7f96e6946adb26794b0d22fd26da
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-05-16 10:58:28 -0400

    created migrations, database map bug

---
+++ db/migrate/20210516142426_create_favorite_quotes.rb
@@ -0,0 +1,9 @@
+class CreateFavoriteQuotes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :favorite_quotes do |t|
+      t.integer :quote_id
+      t.integer :character_id
+      t.integer :user_id
+    end
+  end
+end
