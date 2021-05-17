82645944a08458ebbdd367124c6fa0845433f7cb
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-05-16 20:06:21 -0400

    attempt at database correction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 require './config/environment'
+require 'sinatra/base'
 require "sinatra/activerecord"
 
 class ApplicationController < Sinatra::Base
+  
+    configure do
+    enable :sessions
+    set :session_secret, 'secret'
+    end
 
 end
--- run.rb
+++ run.rb
@@ -1,4 +1,2 @@
 require_relative './config/environment'
 require 'dotenv/load'
-
-User.create({username: "test", email: "foo@bar.com", password_digest: "foobar"})
