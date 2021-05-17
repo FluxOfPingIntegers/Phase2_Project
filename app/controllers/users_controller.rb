d4b79de11a63e951586a7068871729aa2136975b
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-05-17 15:39:19 -0400

    post /users method redirect for now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
+require 'pry'
+
 class UsersController < ApplicationController
 
   get "/users/new" do
 
     erb :"users/new"
+  end
+
+  post "/users" do
+    user = User.create(params["user"])
+    redirect "/"
   end
 
 end
