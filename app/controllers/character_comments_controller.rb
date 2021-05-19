fc32933608f91141113272377c9e7801aee7150f
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-05-19 09:44:03 -0400

    comment form to characters show

--- app/controllers/character_comments_controller.rb
+++ app/controllers/character_comments_controller.rb
@@ -9,7 +9,13 @@ class CharacterCommentsController < ApplicationController
   end
 
   post '/characters/:character_id/comments' do
-
+    binding.pry
+    comment = params["comment"]
+    comment["character_id"] = comment["character_id"].to_i
+    comment["user_id"] = session[:user_id]
+    comment["timestamp"] = Time.now.to_i
+    Comment.create(comment)
+    redirect "/characters/:id"
   end
 
   get '/characters/:character_id/comments/:id' do
--- app/models/Comment.rb
+++ app/models/Comment.rb
@@ -2,6 +2,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :character
+  validates :content
 
 
 end
