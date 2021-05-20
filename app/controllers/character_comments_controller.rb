808f2cfc64b9fd535892f51b47a8907f1ea94583
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-05-19 23:46:10 -0400

    routes and views completed, images next

--- app/controllers/character_comments_controller.rb
+++ app/controllers/character_comments_controller.rb
@@ -32,20 +32,42 @@ class CharacterCommentsController < ApplicationController
     redirect "/characters/#{comment.character_id}"
   end
 
-  get '/characters/:character_id/comments/:id' do
+  #get '/characters/:character_id/comments/:id' do
 
-  end
+  #end
 
   get '/characters/:character_id/comments/:id/edit' do
-
+    @comment = Comment.find_by(params)
+    if @comment.user_id == session[:user_id]
+      erb :'comments/edit'
+    else
+      redirect '/'
+    end
   end
 
   patch '/characters/:character_id/comments/:id' do
-
+    if Comment.find(params[:id].to_i)
+      comment = Comment.find(params[:id].to_i)
+      user = User.find(comment.user_id)
+      if user == current_user && comment.character_id == params[:character_id].to_i
+        params[:comment][:timestamp] = params[:comment][:timestamp].to_i
+        comment.update(params[:comment])
+        redirect "/characters/#{comment.character_id}/comments"
+      end
+      redirect "/characters/#{comment.character_id}/comments"
+    else
+      redirect "/"
+    end
   end
 
   delete '/characters/:character_id/comments/:id' do
-
+    comment = Comment.find(params[:id].to_i)
+    if current_usr == comment.user && comment.character_id == params[:character_id].to_i
+      comment.destroy
+      redirect "/characters/#{comment.character_id}/comments"
+    else
+      redirect "/user/#{comment.user_id}"
+    end
   end
 
 
