143c6e2d3d8fa1976ede1a499dec3093f2b0b7fd
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-05-25 10:29:53 -0400

    working on quotes

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -15,4 +15,14 @@ class CharactersController < ApplicationController
     end
   end
 
+  get "/characters/:id/quotes" do
+    if logged_in?
+      @character = Character.find(params[:id])
+      @user = User.find(session[:user_id])
+      erb :'quotes/show'
+    else
+      redirect "/login"
+    end
+  end
+
 end
